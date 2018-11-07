d1be383e5c20d3d72b999e2e5d00dfee738c6663
Joceane <joceane.rsilva@gmail.com>
2018-11-07 21:37:42 -0200

    Projeto Csharp
    Criação de projeto

---
+++ projeto_csharp/FormPessoa.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace projeto_csharp
+{
+    partial class FormPessoa
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.labelTitulo = new System.Windows.Forms.Label();
+            this.maskedTextBoxTelefone = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.checkBoxAtivo = new System.Windows.Forms.CheckBox();
+            this.radioButtonFeminino = new System.Windows.Forms.RadioButton();
+            this.radioButtonMasculino = new System.Windows.Forms.RadioButton();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxEndereco = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButtonJuridica = new System.Windows.Forms.RadioButton();
+            this.radioButtonFisica = new System.Windows.Forms.RadioButton();
+            this.maskedTextBoxCnpjCpf = new System.Windows.Forms.MaskedTextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Enabled = false;
+            this.btnSalvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalvar.Location = new System.Drawing.Point(574, 398);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(214, 57);
+            this.btnSalvar.TabIndex = 10;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            // 
+            // labelTitulo
+            // 
+            this.labelTitulo.AutoSize = true;
+            this.labelTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitulo.Location = new System.Drawing.Point(259, 30);
+            this.labelTitulo.Name = "labelTitulo";
+            this.labelTitulo.Size = new System.Drawing.Size(283, 39);
+            this.labelTitulo.TabIndex = 2;
+            this.labelTitulo.Text = "Cadastro Pessoa";
+            this.labelTitulo.Click += new System.EventHandler(this.labelTitulo_Click);
+            // 
+            // maskedTextBoxTelefone
+            // 
+            this.maskedTextBoxTelefone.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskedTextBoxTelefone.Location = new System.Drawing.Point(127, 310);
+            this.maskedTextBoxTelefone.Mask = "(00) 00000-0000";
+            this.maskedTextBoxTelefone.Name = "maskedTextBoxTelefone";
+            this.maskedTextBoxTelefone.Size = new System.Drawing.Size(661, 31);
+            this.maskedTextBoxTelefone.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(17, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 25);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(17, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 25);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Cpf/Cnpj";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(17, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(206, 25);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Data de Nascimento";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(17, 232);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 25);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "E-mail";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(17, 273);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 25);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Endereço";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(17, 315);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(96, 25);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Telefone";
+            // 
+            // checkBoxAtivo
+            // 
+            this.checkBoxAtivo.AutoSize = true;
+            this.checkBoxAtivo.Checked = true;
+            this.checkBoxAtivo.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxAtivo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxAtivo.Location = new System.Drawing.Point(22, 359);
+            this.checkBoxAtivo.Name = "checkBoxAtivo";
+            this.checkBoxAtivo.Size = new System.Drawing.Size(79, 29);
+            this.checkBoxAtivo.TabIndex = 7;
+            this.checkBoxAtivo.Text = "Ativo";
+            this.checkBoxAtivo.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonFeminino
+            // 
+            this.radioButtonFeminino.AutoSize = true;
+            this.radioButtonFeminino.Checked = true;
+            this.radioButtonFeminino.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButtonFeminino.Location = new System.Drawing.Point(507, 359);
+            this.radioButtonFeminino.Name = "radioButtonFeminino";
+            this.radioButtonFeminino.Size = new System.Drawing.Size(118, 29);
+            this.radioButtonFeminino.TabIndex = 8;
+            this.radioButtonFeminino.TabStop = true;
+            this.radioButtonFeminino.Text = "Feminino";
+            this.radioButtonFeminino.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonMasculino
+            // 
+            this.radioButtonMasculino.AutoSize = true;
+            this.radioButtonMasculino.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButtonMasculino.Location = new System.Drawing.Point(638, 359);
+            this.radioButtonMasculino.Name = "radioButtonMasculino";
+            this.radioButtonMasculino.Size = new System.Drawing.Size(128, 29);
+            this.radioButtonMasculino.TabIndex = 9;
+            this.radioButtonMasculino.TabStop = true;
+            this.radioButtonMasculino.Text = "Masculino";
+            this.radioButtonMasculino.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(428, 361);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(61, 25);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Sexo";
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxNome.Location = new System.Drawing.Point(91, 93);
+            this.textBoxNome.MaxLength = 70;
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(697, 31);
+            this.textBoxNome.TabIndex = 1;
+            this.textBoxNome.TextChanged += new System.EventHandler(this.textBoxNome_TextChanged);
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxEmail.Location = new System.Drawing.Point(95, 232);
+            this.textBoxEmail.MaxLength = 70;
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(693, 31);
+            this.textBoxEmail.TabIndex = 4;
+            // 
+            // textBoxEndereco
+            // 
+            this.textBoxEndereco.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxEndereco.Location = new System.Drawing.Point(127, 273);
+            this.textBoxEndereco.MaxLength = 70;
+            this.textBoxEndereco.Name = "textBoxEndereco";
+            this.textBoxEndereco.Size = new System.Drawing.Size(661, 31);
+            this.textBoxEndereco.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButtonJuridica);
+            this.groupBox1.Controls.Add(this.radioButtonFisica);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(574, 125);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(214, 55);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tipo";
+            // 
+            // radioButtonJuridica
+            // 
+            this.radioButtonJuridica.AutoSize = true;
+            this.radioButtonJuridica.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButtonJuridica.Location = new System.Drawing.Point(103, 23);
+            this.radioButtonJuridica.Name = "radioButtonJuridica";
+            this.radioButtonJuridica.Size = new System.Drawing.Size(105, 29);
+            this.radioButtonJuridica.TabIndex = 1;
+            this.radioButtonJuridica.Text = "Jurídica";
+            this.radioButtonJuridica.UseVisualStyleBackColor = true;
+            this.radioButtonJuridica.CheckedChanged += new System.EventHandler(this.radioButtonJuridica_CheckedChanged);
+            // 
+            // radioButtonFisica
+            // 
+            this.radioButtonFisica.AutoSize = true;
+            this.radioButtonFisica.Checked = true;
+            this.radioButtonFisica.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButtonFisica.Location = new System.Drawing.Point(10, 23);
+            this.radioButtonFisica.Name = "radioButtonFisica";
+            this.radioButtonFisica.Size = new System.Drawing.Size(87, 29);
+            this.radioButtonFisica.TabIndex = 0;
+            this.radioButtonFisica.TabStop = true;
+            this.radioButtonFisica.Text = "Física";
+            this.radioButtonFisica.UseVisualStyleBackColor = true;
+            this.radioButtonFisica.CheckedChanged += new System.EventHandler(this.radioButtonFisica_CheckedChanged);
+            // 
+            // maskedTextBoxCnpjCpf
+            // 
+            this.maskedTextBoxCnpjCpf.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskedTextBoxCnpjCpf.Location = new System.Drawing.Point(123, 140);
+            this.maskedTextBoxCnpjCpf.Name = "maskedTextBoxCnpjCpf";
+            this.maskedTextBoxCnpjCpf.Size = new System.Drawing.Size(445, 31);
+            this.maskedTextBoxCnpjCpf.TabIndex = 2;
+            this.maskedTextBoxCnpjCpf.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBoxCnpjCpf_MaskInputRejected);
+            this.maskedTextBoxCnpjCpf.TextChanged += new System.EventHandler(this.maskedTextBoxCnpjCpf_TextChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(229, 193);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(559, 31);
+            this.dateTimePicker1.TabIndex = 20;
+            // 
+            // FormPessoa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(805, 477);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.maskedTextBoxCnpjCpf);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBoxEndereco);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.textBoxNome);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.radioButtonMasculino);
+            this.Controls.Add(this.radioButtonFeminino);
+            this.Controls.Add(this.checkBoxAtivo);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.maskedTextBoxTelefone);
+            this.Controls.Add(this.labelTitulo);
+            this.Controls.Add(this.btnSalvar);
+            this.MaximizeBox = false;
+            this.Name = "FormPessoa";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro Pessoa";
+            this.Load += new System.EventHandler(this.FormPessoa_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Label labelTitulo;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxTelefone;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox checkBoxAtivo;
+        private System.Windows.Forms.RadioButton radioButtonFeminino;
+        private System.Windows.Forms.RadioButton radioButtonMasculino;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxNome;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.TextBox textBoxEndereco;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButtonJuridica;
+        private System.Windows.Forms.RadioButton radioButtonFisica;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxCnpjCpf;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ projeto_csharp/FormPessoa.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace projeto_csharp
+{
+    public partial class FormPessoa : Form
+    {
+        public FormPessoa()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButtonFisica_CheckedChanged(object sender, EventArgs e)
+        {
+            setarMascaraCpfCnpj(true);
+        }
+
+        private void radioButtonJuridica_CheckedChanged(object sender, EventArgs e)
+        {
+            setarMascaraCpfCnpj(false);
+        }
+
+        private void FormPessoa_Load(object sender, EventArgs e)
+        {
+            setarMascaraCpfCnpj(true);
+            validarCpfCnpjParaPermitirSalvar();
+        }
+
+        private void setarMascaraCpfCnpj(bool tipo)
+        {
+            if (tipo)
+            {//verdadeiro é fisica
+                maskedTextBoxCnpjCpf.Text = "";
+                maskedTextBoxCnpjCpf.Mask = "000,000,000-00";
+            }
+            else
+            {//falso juridica
+                maskedTextBoxCnpjCpf.Text = "";
+                maskedTextBoxCnpjCpf.Mask = "00,000,000/0000-00";
+            }
+        }
+
+        private void maskedTextBoxCnpjCpf_TextChanged(object sender, EventArgs e)
+        {
+            validarCpfCnpjParaPermitirSalvar();
+        }
+
+
+        private void validarCpfCnpjParaPermitirSalvar()
+        {
+            if (radioButtonFisica.Checked)
+            {
+                if (Validacao.IsCpf(maskedTextBoxCnpjCpf.Text))
+                {
+                    btnSalvar.Enabled = true;
+                }
+                else
+                {
+                    btnSalvar.Enabled = false;
+                }
+            }
+
+            if (radioButtonJuridica.Checked)
+            {
+                if (Validacao.IsCnpj(maskedTextBoxCnpjCpf.Text))
+                {
+                    btnSalvar.Enabled = true;
+                }
+                else
+                {
+                    btnSalvar.Enabled = false;
+                }
+            }
+        }
+
+        private void labelTitulo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxNome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void maskedTextBoxCnpjCpf_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
